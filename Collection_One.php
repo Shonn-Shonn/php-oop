2244e16fb13f632d261fbe7045b40f327b8e43c0
Wint Phyusin Maung <sshonn687@gmail.com>
2023-09-29 09:41:22 +0630

    Add: static function

--- Collection_One.php
+++ Collection_One.php
@@ -10,11 +10,35 @@ class Collection_One
         $this->items = $items;
     }
 
+    public static function accessible($value)
+    {
+        return is_array($value) || $value instanceof ArrayAccess;
+    }
+
     function Column(){
         $rec = array_column($this->items, 'shop_name', 'year');
         return $rec;
     }
-     
+
+    public static function add($array, $key, $value)
+    {
+        if (is_null(static::get($array, $key))) {
+            static::set($array, $key, $value);
+        }
+
+        return $array;
+    }
+
+    public static function set(&$array, $key, $value)
+    {
+        $array[$key] = $value;
+    }
+
+    public static function get($array, $key, $default = null)
+    {
+        return isset($array[$key]) ? $array[$key] : $default;
+    }
+
 }
 
 
@@ -31,4 +55,9 @@ class One{
 
 $objs = new Collection_One([new One('BimBam',2002),new One('KFC',2010),new One('Lotteria',2001)]);
 
-print_r($objs->Column());
+//print_r($objs->Column());
+
+
+$newArray = Collection_One::accessible([new One('Bim',23),new One('KF',235)]);
+
+print_r($newArray);
